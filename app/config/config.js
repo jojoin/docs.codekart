c8a615fc2418b2d6494a8a395f6b7b61f2b4187e
yangjie <yangjie@jojoin.com>
2014-07-02 15:09:21 +0800

    config

--- app/config/config.js
+++ app/config/config.js
@@ -17,9 +17,9 @@ module.exports = {
     route: true,                      //是否开启默认路由匹配（在未定义url处理程序的情况下，搜索文件位置查找处理程序）
     cluster: true,                    //是否开启多核支持、守护进程
     worker: 1,                        //开启的worker工作者线程的数量，需要开启上面的cluster才生效，当值为0则与CPU核心数量相同
-    debug: true,                  //是否为调试模式  调试会将错误抛出终止进程
-    compiled: true,               //是否总是编译html、js、css等页面文件（比较耗时，debug时使用）
-    compress: false,            //是否压缩js、css文件（比较耗时，debug时关闭）
-    expires: 0 //100*60*60*24        //静态文件客户端缓存时间
+    debug: false,                  //是否为调试模式  调试会将错误抛出终止进程
+    compiled: false,               //是否总是编译html、js、css等页面文件（比较耗时，debug时使用）
+    compress: true,            //是否压缩js、css文件（比较耗时，debug时关闭）
+    expires: 100*60*60*24        //静态文件客户端缓存时间
 
 };
